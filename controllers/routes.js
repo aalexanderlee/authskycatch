c2a43ca83479aca732a9dd81df41f18734fbc104
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2018-02-10 15:01:38 -0800

    Send to home or signup pages depending on success or failure with passport authentication from the signup page.

--- controllers/routes.js
+++ controllers/routes.js
@@ -5,17 +5,25 @@ var sequelize = require("sequelize");
 
 var db = require("../models");
 
-// The "/" path will lead to landing.js
+// The "/" path will lead to landing.handlebars
 router.get("/", function(req, res) {
   res.render("landing");
 });
 
-// "/signup" path will lead to signup.js
+// "/signup" path will lead to signup.handlebars
 router.get("/signup", function(req, res) {
   res.render("signup");
 });
 
-// "/login" path will lead to login.js
+// "/login" path will lead to login.handlebars
 router.get("/login", function(req, res) {
   res.render("login");
 });
+
+// "/signup" post path will render either home.handlebars or signup.handlebars based on success or failure
+router.post("/signup",
+  passport.authenticate("local-signup", {
+    successRedirect: "/home",
+    failureRedirect: "/signup"
+  })
+);
