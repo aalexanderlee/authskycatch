cd494e539ef86c66a68242efdd79ac8dfccfb82e
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2018-02-10 14:57:25 -0800

    Initialize 'get' routes for landing, login, and signup pages in controllers. These pages will not be rendered after login with correct credentials are complete, and after passport authentication has been satisfied.

--- controllers/routes.js
+++ controllers/routes.js
@@ -4,3 +4,18 @@ var passport = require("passport");
 var sequelize = require("sequelize");
 
 var db = require("../models");
+
+// The "/" path will lead to landing.js
+router.get("/", function(req, res) {
+  res.render("landing");
+});
+
+// "/signup" path will lead to signup.js
+router.get("/signup", function(req, res) {
+  res.render("signup");
+});
+
+// "/login" path will lead to login.js
+router.get("/login", function(req, res) {
+  res.render("login");
+});
